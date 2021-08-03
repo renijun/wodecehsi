e498512153c54d61a5274a18160e240d43ecc999
renyijun <1904814081@qq.com>
2021-08-03 09:12:32 +0800

    主分支

---
+++ app/src/main/java/com/huatian/myapplication/ui/Main2Activity.java
@@ -0,0 +1,16 @@
+package com.huatian.myapplication.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.huatian.myapplication.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
