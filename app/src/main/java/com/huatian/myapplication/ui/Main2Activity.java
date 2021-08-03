89082af1062e4c5aed9818ea8118aba7a698942f
renyijun <1904814081@qq.com>
2021-08-03 10:03:55 +0800

    dev

--- app/src/main/java/com/huatian/myapplication/ui/Main2Activity.java
+++ app/src/main/java/com/huatian/myapplication/ui/Main2Activity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.huatian.myapplication.R;
+import com.huatian.myapplication.dev.DEV;
 
 public class Main2Activity extends AppCompatActivity {
 
+
+    private DEV dev;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        dev=new DEV();
+        dev.getDev();
     }
 }
